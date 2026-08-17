86ac38dc1be4c7bfea4e4c61b804bfccd6658520
pawelgodlewsky-cloud <pawelgodlewsky@gmail.com>
2026-08-17 19:30:37 +0200

    Reset stale guide cache on test1

--- test1/sw.js
+++ test1/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lombardia-v21-carto-slow-scooter";
+const CACHE = "lombardia-v22-carto-cache-reset";
 const LOCAL = [
   "./", "index.html", "styles.css", "app.js", "manifest.webmanifest",
   "assets/bergamo.webp", "assets/como.webp", "assets/milano.webp", "assets/logo-martyna.png", "assets/logo-martyna.webp", "assets/app-icon.svg",
