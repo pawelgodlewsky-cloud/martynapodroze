dcd587c6bf8ea14385bb8a5feba0cc6dee406af8
pawelgodlewsky-cloud <pawelgodlewsky@gmail.com>
2026-08-28 10:06:20 +0200

    release: pin production guide to version 40

--- src/commerce.ts
+++ src/commerce.ts
@@ -6,7 +6,7 @@ const GUIDE_CURRENCY = "pln";
 const MAX_GUIDE_DEVICES = 3;
 const DEVICE_COOKIE = "mp_lombardia_access";
 const TOKEN_TOLERANCE_SECONDS = 300;
-const GITHUB_GUIDE_ROOT = "https://raw.githubusercontent.com/pawelgodlewsky-cloud/martynapodroze/e157ef8e919f7aa1cf9cd3ef00b974488a6b535a/como";
+const GITHUB_GUIDE_ROOT = "https://raw.githubusercontent.com/pawelgodlewsky-cloud/martynapodroze/51430de204e1a90e69461c7e15a3cd74add488d8/como";
 const GUIDE_PREVIEW_ROOT = "/podglad/como/";
 const GUIDE_CSP = [
   "default-src 'self'",
