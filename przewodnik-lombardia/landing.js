2871c9fcc10c2bdbe97d9892416d3123b713de2c
pawelgodlewsky-cloud <pawelgodlewsky@gmail.com>
2026-09-01 19:54:01 +0200

    feat: publish travel blog and guide updates

--- przewodnik-lombardia/landing.js
+++ przewodnik-lombardia/landing.js
@@ -1,5 +1,4 @@
 const CHECKOUT_URL = "https://buy.stripe.com/aFa7sL3Qafq82VZ0pZ2VG00";
-const FALLBACK_CONTACT_URL = "mailto:podroz.martyna@gmail.com?subject=Kupuj%C4%99%20przewodnik%20po%20Lombardii&body=Chc%C4%99%20kupi%C4%87%20przewodnik%20po%20Lombardii%20za%2059%20z%C5%82.%0A%0AProsz%C4%99%20o%20instrukcj%C4%99%20p%C5%82atno%C5%9Bci%20i%20dost%C4%99p.";
 
 function trackGuideEvent(eventName, placement = "page") {
   const detail = { event: eventName, product: "lombardia_guide", placement };
@@ -8,7 +7,7 @@ function trackGuideEvent(eventName, placement = "page") {
 }
 
 document.querySelectorAll("[data-checkout]").forEach((link, index) => {
-  link.href = CHECKOUT_URL || FALLBACK_CONTACT_URL;
+  link.href = CHECKOUT_URL;
   link.addEventListener("click", () => trackGuideEvent("guide_checkout_click", index === 0 ? "hero" : "closing"));
 });
 
